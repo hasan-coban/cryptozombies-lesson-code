1dba145b4b1ce658cf5e1db302f669c2e50be168
hasan-coban <112540143+hasan-coban@users.noreply.github.com>
2023-08-13 14:02:38 +0300

    Update zombiefeeding.sol

--- lesson-2/chapter-7/zombiefeeding.sol
+++ lesson-2/chapter-7/zombiefeeding.sol
@@ -3,7 +3,8 @@ import "./zombiefactory.sol";
 contract ZombieFeeding is ZombieFactory {
 
   function feedAndMultiply(uint _zombieId, uint _targetDna) public {
-    require(msg.sender == zombieToOwner[_zombieId]);
+    require(msg.sender == zombieToOwner[_zombieId]);//to avoid a random user to feed and multiply our zombie, a small check is set here.the caller of this
+                                                    //function should be the owner of the zombie has _zombieId
     Zombie storage myZombie = zombies[_zombieId];
   }
 
