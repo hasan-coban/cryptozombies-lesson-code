5ad81132ec7e0f0ed053f8183c496b36208b4822
hasan-coban <112540143+hasan-coban@users.noreply.github.com>
2023-08-13 14:17:41 +0300

    Update zombiefeeding.sol

--- lesson-2/chapter-11/zombiefeeding.sol
+++ lesson-2/chapter-11/zombiefeeding.sol
@@ -12,11 +12,18 @@ contract KittyInterface {
     uint256 sireId,
     uint256 generation,
     uint256 genes
-  );
+  );/*Notice that this looks like defining a contract, with a few differences. For one, we're only declaring the functions we want to interact with — 
+in this case getKitty — and we don't mention any of the other functions or state variables.
+
+Secondly, we're not defining the function bodies. Instead of curly braces ({ and }), we're simply ending the function declaration with a semi-colon (;).
+
+So it kind of looks like a contract skeleton. This is how the compiler knows it's an interface.By including this interface in our dapp's code 
+our contract knows what the other contract's functions look like, how to call them, and what sort of response to expect.
+*/
 }
 contract ZombieFeeding is ZombieFactory {
 
-  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;//crytoKitties contract address
   KittyInterface kittyContract = KittyInterface(ckAddress);
 
   function feedAndMultiply(uint _zombieId, uint _targetDna) public {
