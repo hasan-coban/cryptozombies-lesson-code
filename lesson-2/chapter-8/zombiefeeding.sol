590bde69d0187e7398d277f382d180cc5209e4b6
hasan-coban <112540143+hasan-coban@users.noreply.github.com>
2023-08-13 14:08:38 +0300

    Update zombiefeeding.sol

--- lesson-2/chapter-8/zombiefeeding.sol
+++ lesson-2/chapter-8/zombiefeeding.sol
@@ -8,6 +8,8 @@ contract ZombieFeeding is ZombieFactory {
     _targetDna = _targetDna % dnaModulus;//in case _targetDna is more than 16 digits, modulus is used 
     uint newDna = (myZombie.dna + _targetDna) / 2;//since myZombie is a struct , we can get its dna, by dot notation here
     _createZombie("NoName", newDna); //calling the function and passing new zombie name as "NoName" and its dna as newDna
+//A PROBLEM with this calling is that the function's visibility is set to private in zombiefactory.sol, a private function can not be called by other 
+  //contracts in solidity. Need to be fixed
   }
 
 }
