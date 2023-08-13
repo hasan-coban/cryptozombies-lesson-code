1a67993eb4b0697a85dc2dd53e45866b5e01ef64
hasan-coban <112540143+hasan-coban@users.noreply.github.com>
2023-08-13 14:04:51 +0300

    Update zombiefeeding.sol

--- lesson-2/chapter-8/zombiefeeding.sol
+++ lesson-2/chapter-8/zombiefeeding.sol
@@ -5,9 +5,9 @@ contract ZombieFeeding is ZombieFactory {
   function feedAndMultiply(uint _zombieId, uint _targetDna) public {
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
-    _targetDna = _targetDna % dnaModulus;
-    uint newDna = (myZombie.dna + _targetDna) / 2;
-    _createZombie("NoName", newDna);
+    _targetDna = _targetDna % dnaModulus;//in case _targetDna is more than 16 digits, modulus is used 
+    uint newDna = (myZombie.dna + _targetDna) / 2;//since myZombie is a struct , we can get its dna, by dot notation here
+    _createZombie("NoName", newDna); //calling the function and passing new zombie name as "NoName" and its dna as newDna
   }
 
 }
