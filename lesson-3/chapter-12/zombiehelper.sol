f4f65cbdd97314a591354ef6319a2f0c2b022ddf
hasan-coban <112540143+hasan-coban@users.noreply.github.com>
2023-08-14 08:56:35 +0300

    Update zombiehelper.sol

--- lesson-3/chapter-12/zombiehelper.sol
+++ lesson-3/chapter-12/zombiehelper.sol
@@ -20,8 +20,8 @@ contract ZombieHelper is ZombieFeeding {
   function getZombiesByOwner(address _owner) external view returns(uint[]) {
     uint[] memory result = new uint[](ownerZombieCount[_owner]);
     uint counter = 0;
-    for (uint i = 0; i < zombies.length; i++) {
-      if (zombieToOwner[i] == _owner) {
+    for (uint i = 0; i < zombies.length; i++) {//filling the result array by iterating over zombies arrays
+      if (zombieToOwner[i] == _owner) { //zombieToOwner[i] is gonna map to an adress, and we check if that's same with _owner
         result[counter] = i;
         counter++;
       }
