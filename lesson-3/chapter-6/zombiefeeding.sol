e872175001e1eed3d93c7ceb4f36277598fa846b
hasan-coban <112540143+hasan-coban@users.noreply.github.com>
2023-08-14 08:31:33 +0300

    Update zombiefeeding.sol

--- lesson-3/chapter-6/zombiefeeding.sol
+++ lesson-3/chapter-6/zombiefeeding.sol
@@ -27,7 +27,7 @@ contract ZombieFeeding is ZombieFactory {
   }
 
   function _isReady(Zombie storage _zombie) internal view returns (bool) {
-      return (_zombie.readyTime <= now);
+      return (_zombie.readyTime <= now);//checking if enough time has passed before new feeding
   }
 
   function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) public {
